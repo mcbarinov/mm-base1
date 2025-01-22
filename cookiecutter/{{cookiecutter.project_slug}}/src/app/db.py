5170f6a197e23ccc67e6f3dfaef92ee1a462d791
mcbarinov <mcbarinov@proton.me>
2025-01-22 18:30:32 +0300

    demo and cookiecutter

--- cookiecutter/{{cookiecutter.project_slug}}/src/app/db.py
+++ cookiecutter/{{cookiecutter.project_slug}}/src/app/db.py
@@ -1,10 +1,14 @@
+from typing import TYPE_CHECKING
+
 from mm_base1.db import BaseDB, DatabaseAny
-from mm_mongo import MongoCollection
 
 from app.models import Data
 
+if TYPE_CHECKING:
+    from mm_mongo import MongoCollection
+
 
 class DB(BaseDB):
-    def __init__(self, database: DatabaseAny):
+    def __init__(self, database: DatabaseAny) -> None:
         super().__init__(database)
         self.data: MongoCollection[Data] = Data.init_collection(database)
